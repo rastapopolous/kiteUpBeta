9b3d6ab5a491188ff6be81608d54608a9427d518
rastapopolous <jdlazar2011@gmail.com>
2016-07-22 17:36:19 -0400

    Jlazar user user profile fields component (#3)
    * user-signup-component-July15
    
    * small tweaks to profile component
    
    * added firstname and lastname
    
    * added userGridIntegration
    
    * revised userGridIntegration
    
    * added dropdown and changed state to province
    
    * pr feedback

--- imports/ui/features/UserProfile/index.js
+++ imports/ui/features/UserProfile/index.js
@@ -1,9 +1,102 @@
 import React, { Component } from 'react'
 
 export default class UserProfile extends Component {
+
+  constructor () {
+    super()
+
+    this.state = {
+      firstname: '',
+      lastname: '',
+      email: '',
+      province: '',
+      city: '',
+      phone: '',
+      skill: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    this.setState({
+      firstname: nextProps.user.firstname,
+      lastname: nextProps.user.lastname,
+      province: nextProps.user.province,
+      city: nextProps.user.city,
+      phone: nextProps.user.phone,
+      skill: nextProps.user.skill
+    })
+  }
+
+  handleChange (e) {
+    e.preventDefault()
+
+    this.setState({
+      firstname: this.refs.firstnameInput.value,
+      lastname: this.refs.lastnameInput.value,
+      email: this.refs.emailInput.value,
+      province: this.refs.provinceInput.value,
+      city: this.refs.cityInput.value,
+      phone: this.refs.phoneInput.value,
+      skill: this.refs.skillInput.value
+    })
+  }
+
   render () {
     return (
-      <h2>User Profile</h2>
+      <div>
+        <form
+          className='pure-form pure-form-stacked'>
+          <input
+            type='text'
+            autoFocus
+            value={this.state.firstname}
+            ref='firstnameInput'
+            onChange={this.handleChange}
+            placeholder='Firstname' />
+          <input
+            type='text'
+            value={this.state.lastname}
+            ref='lastnameInput'
+            onChange={this.handleChange}
+            placeholder='Lastname' />
+          <input
+            type='email'
+            value={this.state.email}
+            ref='emailInput'
+            onChange={this.handleChange}
+            placeholder='diego@kiteup.com' />
+          <input
+            type='text'
+            value={this.state.province}
+            ref='provinceInput'
+            onChange={this.handleChange}
+            placeholder='Province' />
+          <input
+            type='text'
+            value={this.state.city}
+            ref='cityInput'
+            onChange={this.handleChange}
+            placeholder='City' />
+          <input
+            type='text'
+            value={this.state.phone}
+            ref='phoneInput'
+            onChange={this.handleChange}
+            placeholder='Phone Number' />
+          <select
+            type='text'
+            value={this.state.skill}
+            ref='skillInput'
+            onChange={this.handleChange}
+            placeholder={'Choose skill level'} >
+            <option>Low</option>
+            <option>Medium</option>
+            <option>High</option>
+          </select>
+        </form>
+      </div>
     )
   }
 }
