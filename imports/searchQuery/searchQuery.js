3060e40d32c541c0803d1a7bd908efb9011d8bfe
Josh Lazar <josh.lazar@ahold.com>
2017-11-22 16:28:56 -0500

    mods to searchQuery

---
+++ imports/searchQuery/searchQuery.js
@@ -0,0 +1,29 @@
+handleSubmit (e) {
+  e.preventDefault()
+  const filteredUsers = this.filterUsers(this.props.users, this.state.keyword)
+  this.props.updatedFilteredUsers(filteredUsers)
+}
+
+filterUsers (users, keyword) {
+  // when the keyword is empty return the full users collection
+  if (keyword.length === 0) {
+    return users
+  }
+
+  const lowecaseKeyword = keyword.toLowerCase()
+  // return the filtered collection of users
+  return users.filter((user) => {
+    let match = false
+
+    Object.keys(user).forEach((key) => {
+      if (!match) {
+        let value = user[key]
+
+        if (typeof value === 'string') {
+          value = value.toLowerCase()
+          match = value.includes(lowecaseKeyword)
+        }
+      }
+    })
+    return match
+  })
